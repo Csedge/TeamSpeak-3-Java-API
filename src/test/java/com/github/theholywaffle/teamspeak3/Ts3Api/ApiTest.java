78bd8c66d7b61827e0c2fe415b9198289d6e517e
Csedge <liangfen@oregonstate.edu>
2019-02-07 21:39:17 -0800

    Latest_Test

--- src/test/java/com/github/theholywaffle/teamspeak3/Ts3Api/ApiTest.java
+++ src/test/java/com/github/theholywaffle/teamspeak3/Ts3Api/ApiTest.java
@@ -1,6 +1,24 @@
 package com.github.theholywaffle.teamspeak3.Ts3Api;
 
 import com.github.theholywaffle.teamspeak3.TS3Api;
+import com.github.theholywaffle.teamspeak3.TS3Query;
+import com.github.theholywaffle.teamspeak3.TS3Config;
+import com.github.theholywaffle.teamspeak3.api.ChannelProperty;
+
+import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -10,33 +28,137 @@ public class ApiTest {
     public ExpectedException thrown = ExpectedException.none();
     @Test
     public void Testmovefile(){
-        TS3Api obj = new TS3Api();
-        obj.moveFile("/dir1/a.txt","/dir2/b.txt",1);
-        obj.moveFile("/dir1/b.txt","/dir2/b.txt",1,2);
+        final TS3Config config = new TS3Config();
+        config.setHost("127.0.0.1");
+        config.setEnableCommunicationsLogging(true);
+        final TS3Query query = new TS3Query(config);
+        query.connect();
+        TS3Api obj = query.getApi();
+        obj.login("serveradmin","FyXmnFvP");
+       obj.selectVirtualServerById(1);
+       obj.moveFile("/dir1/1.txt","/dir2/aasdd.txt",1);
     }
+    @Test
     public void Testmovequery(){
-        TS3Api obj = new TS3Api();
-        obj.moveQuery(1);
+        final TS3Config config = new TS3Config();
+        config.setHost("127.0.0.1");
+        config.setEnableCommunicationsLogging(true);
+        final TS3Query query = new TS3Query(config);
+        query.connect();
+        TS3Api obj = query.getApi();
+        obj.login("serveradmin","FyXmnFvP");
+        obj.selectVirtualServerById(1);
+        obj.moveQuery(2);
     }
+    @Test
     public void TestpokeClient(){
-        TS3Api obj = new TS3Api();
+        final TS3Config config = new TS3Config();
+        config.setHost("127.0.0.1");
+        config.setEnableCommunicationsLogging(true);
+        final TS3Query query = new TS3Query(config);
+        query.connect();
+        TS3Api obj = query.getApi();
+        obj.login("serveradmin","FyXmnFvP");
+        obj.selectVirtualServerById(1);
         obj.pokeClient(1,"Hello World!");
-
-    }
-    public void TestRemoveClientFromServerGroup(){
-        TS3Api obj = new TS3Api();
-        obj.removeClientFromServerGroup(1,2);
     }
 
+    @Test
     public void Testrenamechannelgroup(){
-        TS3Api obj = new TS3Api();
+        final TS3Config config = new TS3Config();
+        config.setHost("127.0.0.1");
+        config.setEnableCommunicationsLogging(true);
+        final TS3Query query = new TS3Query(config);
+        query.connect();
+        TS3Api obj = query.getApi();
+        obj.login("serveradmin","FyXmnFvP");
+        obj.selectVirtualServerById(1);
         obj.renameChannelGroup(1,"Hello");
-        obj.renameChannelGroup("Hello","Hello_new");
+
+    }
+    @Test
+    public void TestsendChannelMsg(){
+        final TS3Config config = new TS3Config();
+        config.setHost("127.0.0.1");
+        config.setEnableCommunicationsLogging(true);
+        final TS3Query query = new TS3Query(config);
+        query.connect();
+        TS3Api obj = query.getApi();
+        obj.login("serveradmin","FyXmnFvP");
+        obj.selectVirtualServerById(1);
+        obj.sendChannelMessage("ChannelMsgTest");
+    }
+    @Test
+    public void TestSendPrivateMessage(){
+        final TS3Config config = new TS3Config();
+        config.setHost("127.0.0.1");
+        config.setEnableCommunicationsLogging(true);
+        final TS3Query query = new TS3Query(config);
+        query.connect();
+        TS3Api obj = query.getApi();
+        obj.selectVirtualServerById(1);
+        obj.login("serveradmin","FyXmnFvP");
+        obj.sendPrivateMessage(1,"Hello");
+    }
+    @Test
+    public void TestSendSeverMsg(){
+        final TS3Config config = new TS3Config();
+        config.setHost("127.0.0.1");
+        config.setEnableCommunicationsLogging(true);
+        final TS3Query query = new TS3Query(config);
+        query.connect();
+        TS3Api obj = query.getApi();
+        obj.selectVirtualServerById(1);
+        obj.login("serveradmin","FyXmnFvP");
+        obj.sendServerMessage("Server Msg Test");
+    }
+    @Test
+    public void TestSetMsgRed(){
+        final TS3Config config = new TS3Config();
+        config.setHost("127.0.0.1");
+        config.setEnableCommunicationsLogging(true);
+        final TS3Query query = new TS3Query(config);
+        query.connect();
+        TS3Api obj = query.getApi();
+        obj.login("serveradmin","FyXmnFvP");
+        //obj.setMessageRead("100");
+    }
+    @Test
+    public void TestSetNickname(){
+        final TS3Config config = new TS3Config();
+        config.setHost("127.0.0.1");
+        config.setEnableCommunicationsLogging(true);
+        final TS3Query query = new TS3Query(config);
+        query.connect();
+        TS3Api obj = query.getApi();
+        obj.login("serveradmin","FyXmnFvP");
+        obj.selectVirtualServerById(1);
+        obj.setNickname("NewNickName");
+    }
+    @Test
+    public void TestUploadFiles(){
+        final TS3Config config = new TS3Config();
+        config.setHost("127.0.0.1");
+        config.setEnableCommunicationsLogging(true);
+        final TS3Query query = new TS3Query(config);
+        query.connect();
+        TS3Api obj = query.getApi();
+        obj.selectVirtualServerById(1);
+        obj.login("serveradmin","FyXmnFvP");
+        byte[] EXAMPLE_FILE_CONTENT = "Hello file transferring world!".getBytes(StandardCharsets.UTF_8);
+        obj.uploadFileDirect(EXAMPLE_FILE_CONTENT,"/dir1/TestUploadeFiles",true,1);
+
+    }
+   @Test
+    public void TestStopServer(){
+        final TS3Config config = new TS3Config();
+        config.setHost("127.0.0.1");
+        config.setEnableCommunicationsLogging(true);
+        final TS3Query query = new TS3Query(config);
+        query.connect();
+        TS3Api obj = query.getApi();
+        obj.login("serveradmin","FyXmnFvP");
+        obj.stopServer(1,"TestStopServer");
     }
 
-    public void TestrenameserverGroup(){
-        TS3Api obj = new TS3Api();
-        obj.renameServerGroup(1,"HelloTestCase");
-        obj.renameServerGroup("HelloTestCase","New!");
-    }
 }
