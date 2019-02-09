8dbd421bf85e178e9bbc6d2577e98712b3c75748
agoschie <goschiea@oregonstate.edu>
2019-02-08 17:02:26 -0800

    more tinkering for the command packaging tests

--- src/test/java/manualTestUnits/manual_tests.java
+++ src/test/java/manualTestUnits/manual_tests.java
@@ -17,8 +17,11 @@ public class manual_tests {
 	@Test
 	public void testCommandBuilder()
 	{
+		//command builder and test integer for iterating through character codes
 		CommandBuilder cmdBld = null;
 		int testInt = 9;
+		
+		//lines 23 - 33 connect a test client to the server
 		final TS3Config config = new TS3Config();
 		config.setHost("localhost");
 
@@ -30,30 +33,50 @@ public class manual_tests {
 		api.selectVirtualServerById(1);
 		api.setNickname("PutPutBot");
 		
+		//make a string for use in tests
 		String str = new String(String.valueOf(Character.toChars(11)));
 		
+		
+		//loop through all valid character code points, testInt gets incremented each time.
 		while(Character.isValidCodePoint(testInt))
 		{
+			//build the command from the ground up
 			cmdBld = new CommandBuilder("sendtextmessage");
 			cmdBld.addIf(true, new KeyValueParam("targetmode", TextMessageTargetMode.CHANNEL.getIndex()));
 			cmdBld.addIf(true, new KeyValueParam("target", 0));
-			cmdBld.addIf(true, new KeyValueParam("msg", String.valueOf(Character.toChars(testInt))));
+			cmdBld.addIf(true, new KeyValueParam("msg", String.valueOf(Character.toChars(testInt)))); //use the string representation of the char code
 			
-			if(cmdBld.build().toString().equals("sendtextmessage targetmode=" + String.valueOf(TextMessageTargetMode.CHANNEL.getIndex()) + " target=0 msg=" + String.valueOf(Character.toChars(testInt)))) 
-			{
-				testInt = testInt + 1;
-			}
-			else
+			//if an altered command, print results
+			if(!(cmdBld.build().toString().equals("sendtextmessage targetmode=" + String.valueOf(TextMessageTargetMode.CHANNEL.getIndex()) + " target=0 msg=" + String.valueOf(Character.toChars(testInt)))))
 			{
 				System.out.print("" + testInt + "\n");
 				System.out.print(String.valueOf(Character.toChars(testInt)) + "\n");
 				System.out.print(cmdBld.build().toString() + "\n");
 				
-				query.getAsyncApi().executeAndReturnError(cmdBld.build()).getUninterruptibly();
-				//api.sendChannelMessage(String.valueOf(Character.toChars(testInt)));
-				
-				testInt = testInt + 1;
+				try
+				{
+					query.getAsyncApi().executeAndReturnError(cmdBld.build()).getUninterruptibly();
+					//api.sendChannelMessage(String.valueOf(Character.toChars(testInt)));
+				}
+				catch(Exception e)
+				{
+					System.out.print(e.getMessage());
+				}
 			}
+			else
+			{
+				//unaltered command, only print information if it failed as result
+				try
+				{
+					query.getAsyncApi().executeAndReturnError(cmdBld.build()).getUninterruptibly();
+				}
+				catch(Exception e)
+				{
+					System.out.print("" + testInt + "\n");
+					System.out.print(e.getMessage());
+				}
+			}
+			testInt = testInt + 1;
 		}
 		
 		
