46410b94ca4b774c3a6b66c7c0322ef2d6ff0bc4
agoschie <goschiea@oregonstate.edu>
2019-03-05 15:44:59 -0800

    added more unit tests

--- src/test/java/unitTests/EventManagerTest.java
+++ src/test/java/unitTests/EventManagerTest.java
@@ -4,13 +4,103 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.github.theholywaffle.teamspeak3.TS3Api;
+import com.github.theholywaffle.teamspeak3.TS3Config;
 import com.github.theholywaffle.teamspeak3.TS3Query;
+import com.github.theholywaffle.teamspeak3.api.event.ChannelCreateEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ChannelDeletedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ChannelDescriptionEditedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ChannelEditedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ChannelMovedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ChannelPasswordChangedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ClientJoinEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ClientLeaveEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ClientMovedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.PrivilegeKeyUsedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ServerEditedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.TS3Listener;
+import com.github.theholywaffle.teamspeak3.api.event.TextMessageEvent;
 
 public class EventManagerTest {
 
 	@Test
-	public void emptyQuery() {
-		
+	public void addEventListenersTest() {
+		final TS3Config config = new TS3Config();
+		config.setHost("localhost");
+		config.setEnableCommunicationsLogging(true);
+
+		final TS3Query query = new TS3Query(config);
+		query.connect();
+
+		final TS3Api api = query.getApi();
+		api.login("serveradmin", "1234");
+		api.selectVirtualServerById(1);
+		api.setNickname("PutPutBot");
+		api.sendChannelMessage("PutPutBot is online!");
+
+		api.registerAllEvents();
+		api.addTS3Listeners(new TS3Listener() {
+
+			@Override
+			public void onTextMessage(TextMessageEvent e) {
+				System.out.println("Text message received in " + e.getTargetMode());
+			}
+
+			@Override
+			public void onServerEdit(ServerEditedEvent e) {
+				System.out.println("Server edited by " + e.getInvokerName());
+			}
+
+			@Override
+			public void onClientMoved(ClientMovedEvent e) {
+				System.out.println("Client has been moved " + e.getClientId());
+			}
+
+			@Override
+			public void onClientLeave(ClientLeaveEvent e) {
+				// ...
+			}
+
+			@Override
+			public void onClientJoin(ClientJoinEvent e) {
+				// ...
+			}
+
+			@Override
+			public void onChannelEdit(ChannelEditedEvent e) {
+				// ...
+			}
+
+			@Override
+			public void onChannelDescriptionChanged(ChannelDescriptionEditedEvent e) {
+				// ...
+			}
+
+			@Override
+			public void onChannelCreate(ChannelCreateEvent e) {
+				// ...
+			}
+
+			@Override
+			public void onChannelDeleted(ChannelDeletedEvent e) {
+				// ...
+			}
+
+			@Override
+			public void onChannelMoved(ChannelMovedEvent e) {
+				// ...
+			}
+
+			@Override
+			public void onChannelPasswordChanged(ChannelPasswordChangedEvent e) {
+				// ...
+			}
+
+			@Override
+			public void onPrivilegeKeyUsed(PrivilegeKeyUsedEvent e) {
+				// ...
+			}
+		});
 		
 	}
 }
--- src/test/java/unitTests/TS3QueryTest.java
+++ src/test/java/unitTests/TS3QueryTest.java
@@ -4,7 +4,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import com.github.theholywaffle.teamspeak3.*;
+import com.github.theholywaffle.teamspeak3.api.exception.TS3QueryShutDownException;
 
 public class TS3QueryTest {
 	@Test
@@ -36,6 +39,7 @@ public class TS3QueryTest {
 	public void QueryWithCustomConfig() {
 		TS3Config config = new TS3Config();
 		TS3Query query;
+		Exception excp = null;
 		
 		config.setHost("localhost");
 		config.setEnableCommunicationsLogging(true);
@@ -52,5 +56,130 @@ public class TS3QueryTest {
 		
 		query.exit();
 		
+		assertFalse(query.isConnected());
+		
+		try {
+			query.connect();
+		} catch(Exception e) {
+			excp = e;
+			System.out.print("GOOD: Could not connect\n");
+		}
+		assertNotNull(excp);
+	}
+	
+	@Test
+	public void TwoQueryWithCustomConfig() {
+		//this tests any shared resources across different instances of query
+		TS3Config config = new TS3Config();
+		TS3Query query;
+		TS3Query query2;
+		Exception excp = null;
+		
+		config.setHost("localhost");
+		config.setEnableCommunicationsLogging(true);
+		
+		query = new TS3Query(config);
+		query2 = new TS3Query(config);
+
+		
+		assertFalse(query.isConnected());
+		assertFalse(query2.isConnected());
+		
+		query.connect();
+		
+		assertTrue(query.isConnected());
+		assertFalse(query2.isConnected());
+		
+		query2.connect();
+		query.exit();
+		query2.connect(); //its behavior is intended to allow multiple connects, refreshing the QueryIO
+		
+		assertFalse(query.isConnected());
+		assertTrue(query2.isConnected());
+		
+		try {
+			query.connect();
+		} catch(Exception e) {
+			excp = e;
+			System.out.print("GOOD: Could not connect\n");
+		}
+		
+		assertNotNull(excp);
+		excp = null;
+		
+		try {
+			query2.connect();
+		} catch(Exception e) {
+			excp = e;
+			System.out.print("BAD: Could not connect\n");
+		}
+		assertNull(excp);
+	}
+	
+	@Test
+	public void MultithreadQueryWithCustomConfig() {
+		//this tests any shared resources across different instances of query
+		TS3Config config = new TS3Config();
+		final TS3Query query;
+		ArrayList<Thread> threads = new ArrayList<Thread>();
+		Thread thread = null;
+		
+		config.setHost("localhost");
+		config.setEnableCommunicationsLogging(true);
+		
+		query = new TS3Query(config);
+		
+		
+		for(int i = 0; i < 100; i = i + 1)
+		{
+			thread = new Thread() {
+				TS3Query q = query;
+				@Override
+				public void run()
+				{
+					q.connect();
+				}
+			};
+			thread.start();
+			threads.add(thread);
+		}
+		
+		for(Thread tmp : threads)
+		{
+			try {
+			tmp.join();
+			} catch (Exception e) {
+				System.out.print(e.getMessage() + "\n");
+			}
+		}
+		
+		threads.clear();
+		
+		query.getApi().login("serveradmin", "1234");
+		
+		for(int i = 0; i < 100; i = i + 1)
+		{
+			thread = new Thread() {
+				TS3Query q = query;
+				@Override
+				public void run()
+				{
+					q.getApi().selectVirtualServerById(1);
+					q.getApi().sendChannelMessage("DERP");
+					q.exit();
+				}
+			};
+			thread.start();
+			threads.add(thread);
+		}
+		
+		for(Thread tmp : threads)
+		{
+			try {
+			tmp.join();
+			} catch (Exception e) {
+				System.out.print(e.getMessage() + "\n");
+			}
+		}
 	}
 }
